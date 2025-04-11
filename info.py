01ec272c5c5a2f18f6e0ef3223b1ea0c39a687cd
dvlnaruto06 <koshtiharshil006@gmail.com>
2025-04-11 23:54:22 +0530

    Update info.py

--- info.py
+++ info.py
@@ -23,7 +23,6 @@ API_HASH = environ.get('API_HASH', '')
 BOT_TOKEN = environ.get('BOT_TOKEN', '')
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6964203412 1886258592').split()]
 USERNAME = environ.get('USERNAME', 'https://telegram.me/dvl_naruto_06')
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002583655280'))
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002153653982').split()]
 DATABASE_URI = environ.get('DATABASE_URI', "")
 DATABASE_URI2 = environ.get('DATABASE_URI2', "")
